dd7e31c5e5a6ac099f51ec57f15b26a39999bc73
lqfdemon <lqfdemon@163.com>
2016-06-27 11:10:44 +0800

    second

--- health_card/views.py
+++ health_card/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(u"欢迎访问香城卫计!")
+    return HttpResponse(u"欢迎访问香城卫计!  你好")
 # Create your views here.
