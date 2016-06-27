b54c63bf87f8ae37b4c09fe5bb94a697da05b0eb
lqfdemon <lqfdemon@163.com>
2016-06-27 10:37:42 +0800

    firmst commit

---
+++ health_card/views.py
@@ -0,0 +1,7 @@
+#-*-coding:UTF-8-*-
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse(u"欢迎访问香城卫计!")
+# Create your views here.
